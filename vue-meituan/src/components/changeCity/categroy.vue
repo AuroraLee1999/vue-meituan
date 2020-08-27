a67e3f1e02a0a526f2abf7046c8357cbb56c91c5
AuroraLee1999 <AuroraLee1999@163.com>
2020-08-27 11:24:00 +0800

    7

--- vue-meituan/src/components/changeCity/categroy.vue
+++ vue-meituan/src/components/changeCity/categroy.vue
@@ -4,6 +4,10 @@
       <dt>按拼音首字母选择：</dt>
       <dd v-for="item in alphabet" :key="item">{{item}}</dd>
     </dl>
+    <dl v-for="(item,index) in cityList" :key="index">
+      <dt>{{item.initials}}</dt>
+      <dd>{{item.cityName}}</dd>
+    </dl>
   </div>
 </template>
 
@@ -11,7 +15,43 @@
 export default {
   data() {
     return {
-      alphabet:'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+      alphabet:'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
+      cityList:[{
+        cityName:"鞍山",
+        initials:'A',
+        abbr:"as",
+        rank:"D"
+      },{
+        cityName:"北京",
+        initials:'B',
+        abbr:"bj",
+        rank:"S"
+      },{
+        cityName:"成都",
+        initials:'C',
+        abbr:"cd",
+        rank:"A"
+      },{
+        cityName:"广州",
+        initials:'G',
+        abbr:"gz",
+        rank:"A"
+      },{
+        cityName:"扬州",
+        initials:'Y',
+        abbr:"yz",
+        rank:"B"
+      },{
+        cityName:"南京",
+        initials:'N',
+        abbr:"nj",
+        rank:"A"
+      },{
+        cityName:"重庆",
+        initials:'C',
+        abbr:"cq",
+        rank:"A"
+      },]
     };
   }
 };
