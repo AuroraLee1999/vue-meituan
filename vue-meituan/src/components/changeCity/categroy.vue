603101ddd283818899eb7d4499a93f0af754fc5a
AuroraLee1999 <AuroraLee1999@163.com>
2020-08-27 13:12:20 +0800

    8

--- vue-meituan/src/components/changeCity/categroy.vue
+++ vue-meituan/src/components/changeCity/categroy.vue
@@ -2,11 +2,15 @@
   <div class="categroy">
     <dl class="my-categroy">
       <dt>按拼音首字母选择：</dt>
-      <dd v-for="item in alphabet" :key="item">{{item}}</dd>
+      <dd v-for="item in alphabet" :key="item">
+        <a :href="'#city-'+item">{{item}}</a>
+      </dd>
     </dl>
-    <dl v-for="(item,index) in cityList" :key="index">
-      <dt>{{item.initials}}</dt>
-      <dd>{{item.cityName}}</dd>
+    <dl v-for="(item, index) in cityGroup" :key="index" class="my-categroy-section" :id="'city-'+index">
+      <dt>{{index}}</dt>
+      <dd >
+        <span v-for="(city, index) in item" :key="index" >{{city.cityName}}</span>
+      </dd>
     </dl>
   </div>
 </template>
@@ -15,48 +19,81 @@
 export default {
   data() {
     return {
-      alphabet:'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
-      cityList:[{
-        cityName:"鞍山",
-        initials:'A',
-        abbr:"as",
-        rank:"D"
-      },{
-        cityName:"北京",
-        initials:'B',
-        abbr:"bj",
-        rank:"S"
-      },{
-        cityName:"成都",
-        initials:'C',
-        abbr:"cd",
-        rank:"A"
-      },{
-        cityName:"广州",
-        initials:'G',
-        abbr:"gz",
-        rank:"A"
-      },{
-        cityName:"扬州",
-        initials:'Y',
-        abbr:"yz",
-        rank:"B"
-      },{
-        cityName:"南京",
-        initials:'N',
-        abbr:"nj",
-        rank:"A"
-      },{
-        cityName:"重庆",
-        initials:'C',
-        abbr:"cq",
-        rank:"A"
-      },]
+      alphabet: "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""),
+      cityGroup: {}
     };
+  },
+  created() {
+    var cityList = [
+      {
+        cityName: "鞍山",
+        pinyin: "anshan",
+        initials: "A",
+        abbr: "as",
+        rank: "D"
+      },
+      {
+        cityName: "北京",
+        pinyin: "beijing",
+        initials: "B",
+        abbr: "bj",
+        rank: "S"
+      },
+      {
+        cityName: "成都",
+        pinyin: "chengdu",
+        initials: "C",
+        abbr: "cd",
+        rank: "A"
+      },
+      {
+        cityName: "广州",
+        pinyin: "guangzhou",
+        initials: "G",
+        abbr: "gz",
+        rank: "A"
+      },
+      {
+        cityName: "扬州",
+        pinyin: "yangzhou",
+        initials: "Y",
+        abbr: "yz",
+        rank: "B"
+      },
+      {
+        cityName: "南京",
+        pinyin: "nanjing",
+        initials: "N",
+        abbr: "nj",
+        rank: "A"
+      },
+      {
+        cityName: "重庆",
+        pinyin: "chongqing",
+        initials: "C",
+        abbr: "cq",
+        rank: "A"
+      },
+      {
+        cityName: "上海",
+        pinyin: "shanghai",
+        initials: "S",
+        abbr: "sh",
+        rank: "S"
+      }
+    ];
+    var cityObj = {};
+    cityList.forEach((item, index) => {
+      if (!cityObj[item.initials]) {
+        cityObj[item.initials] = [];
+      }
+      cityObj[item.initials].push(item);
+    });
+    this.cityGroup = cityObj;
   }
 };
 </script>
 
 <style lang='scss'>
-@import '@/assets/css/changecity/categroy.scss'
+@import "@/assets/css/changecity/categroy.scss";
 </style>
